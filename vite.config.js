d113d2e481d5af37340aedb4e1f6add03832f2e6
manager <victorfeitosa0110@gmail.com>
2025-09-06 01:13:29 -0400

    initial

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+const { defineConfig, splitVendorChunkPlugin } = require("vite");
+const react = require("@vitejs/plugin-react-swc");
+const config = require("vite-jsconfig")
+
+module.exports = defineConfig({
+  plugins: [react(), splitVendorChunkPlugin(), config()],
+  server:{
+    open: true,
+    host: true,
+  },
+  hmr: {
+      overlay: false,
+  }
+});
