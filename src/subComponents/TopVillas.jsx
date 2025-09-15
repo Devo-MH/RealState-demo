9aa81950c33a02d06aba744d73894b6527d57bc9
manager <victorfeitosa0110@gmail.com>
2025-09-14 22:18:00 -0400

    prettier

--- src/subComponents/TopVillas.jsx
+++ src/subComponents/TopVillas.jsx
@@ -8,59 +8,64 @@ import { FaBath } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 
 const TopVillas = () => {
-  return (
-    <section id="topVillas">
-      <h1>TOP PICK VILLAS</h1>
-      <p>
-        Discover our handpicked selection of villas offering the perfect blend of 
-        luxury, comfort, and breathtaking locations. Each villa is thoughtfully 
-        designed to provide you with unforgettable stays — whether for family 
-        getaways, romantic escapes, or relaxing retreats.
-      </p>
-      <div className="villasContainer">
-        {villas.slice(0,3).map((element) => {
-          return (
-              <Link to={`/villa/${element.id}`} className="card" key={element.id}>
-                <img src={element.image} alt={element.name} />
-                <div className="location_text">
-                  <span>{element.location}</span>
-                  <span>
-                    <RxDot />
-                  </span>
-                  <span>{element.category}</span>
-                </div>
-                <div className="title_text">{element.name}</div>
-                <div className="specifications">
-                  <div className="spec">
-                    <IoIosPeople />
-                    <span>{element.guests}</span>
-                    Guests
-                  </div>
-                  <div className="spec">
-                    <FaBed />
-                    <span>{element.bedrooms}</span>
-                    Bedrooms
-                  </div>
-                  <div className="spec">
-                    <BiArea />
-                    <span>{element.squareMeter}</span>
-                    Area
-                  </div>
-                  <div className="spec">
-                    <FaBath />
-                    <span>{element.bathrooms}</span>
-                    Bathrooms
-                  </div>
-                </div>
-                <div className="badge">
-                  From <span>Rs.{element.dailyRent} / Day </span>
-                </div>
-              </Link>
-          );
-        })}
-      </div>
-    </section>
-  );
+    return (
+        <section id="topVillas">
+            <h1>TOP PICK VILLAS</h1>
+            <p>
+                Discover our handpicked selection of villas offering the perfect
+                blend of luxury, comfort, and breathtaking locations. Each villa
+                is thoughtfully designed to provide you with unforgettable stays
+                — whether for family getaways, romantic escapes, or relaxing
+                retreats.
+            </p>
+            <div className="villasContainer">
+                {villas.slice(0, 3).map((element) => {
+                    return (
+                        <Link
+                            to={`/villa/${element.id}`}
+                            className="card"
+                            key={element.id}
+                        >
+                            <img src={element.image} alt={element.name} />
+                            <div className="location_text">
+                                <span>{element.location}</span>
+                                <span>
+                                    <RxDot />
+                                </span>
+                                <span>{element.category}</span>
+                            </div>
+                            <div className="title_text">{element.name}</div>
+                            <div className="specifications">
+                                <div className="spec">
+                                    <IoIosPeople />
+                                    <span>{element.guests}</span>
+                                    Guests
+                                </div>
+                                <div className="spec">
+                                    <FaBed />
+                                    <span>{element.bedrooms}</span>
+                                    Bedrooms
+                                </div>
+                                <div className="spec">
+                                    <BiArea />
+                                    <span>{element.squareMeter}</span>
+                                    Area
+                                </div>
+                                <div className="spec">
+                                    <FaBath />
+                                    <span>{element.bathrooms}</span>
+                                    Bathrooms
+                                </div>
+                            </div>
+                            <div className="badge">
+                                From <span>Rs.{element.dailyRent} / Day </span>
+                            </div>
+                        </Link>
+                    );
+                })}
+            </div>
+        </section>
+    );
 };
 
 export default TopVillas;
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 const { defineConfig, splitVendorChunkPlugin } = require("vite");
 const react = require("@vitejs/plugin-react-swc");
-const config = require("vite-jsconfig")
+const config = require("vite-jsconfig");
 
 module.exports = defineConfig({
-  plugins: [react(), splitVendorChunkPlugin(), config()],
-  server:{
-    open: true,
-    host: true,
-  },
-  hmr: {
-      overlay: false,
-  }
+    plugins: [react(), splitVendorChunkPlugin(), config()],
+    server: {
+        open: true,
+        host: true,
+    },
+    hmr: {
+        overlay: false,
+    },
 });
